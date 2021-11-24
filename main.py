0a745e013a10ddf173db54de64fc472418a36d94
Emine Dogan <emine.dogan@gmail.com>
2021-11-24 15:01:34 -0500

    init converting temp

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# def compute_area():
+#     radius = 20.0
+#     area = radius*radius*3.14159
+#     print("The area of the circle is {} is {} area.".format(radius, area))
+
+
+def convert_temp(f):
+    celcius = (f-32)*(5.0/9.0)
+    return float(celcius)
+
+
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+    # compute_area()
+    # my_value=input("enter your age:  ")
+    # print("my age is {}.".format(my_value))
+
+    result = convert_temp(90)
+    print(result)
+
+
+
+
+
+
+
+
+
